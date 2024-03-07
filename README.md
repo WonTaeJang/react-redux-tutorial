33e770c82ff46fe3cb845a36cc0a00f3baa892b8
WonTaeJang <jwt0212@gmail.com>
2024-03-07 21:48:43 +0900

    redux tutorial decrease increase button

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # React Redux Tutorial
 
-react
-redux
+- React 18
+- Redux [(Tutorial Link)](https://react-redux.js.org/introduction/getting-started)
+
+  - npm install @reduxjs/toolkit react-redux
---
+++ src/features/counter/Counter.jsx
@@ -0,0 +1,28 @@
+
+import { decrement, increment } from './counterSlice'
+import { useDispatch, useSelector } from "react-redux"
+
+export default function Counter () {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  )
+}
